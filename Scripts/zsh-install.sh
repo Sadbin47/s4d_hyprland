1517b6f90e4c02d25c90621599729846da0a05f5
Sadbin47 <23-55187-3@student.aiub.edu>
2026-02-07 23:47:23 +0600

    fix: changing shell to zsh

--- Scripts/zsh-install.sh
+++ Scripts/zsh-install.sh
@@ -17,7 +17,7 @@ install_pkg "starship"
 # Change default shell to Zsh
 if [[ "$SHELL" != "$(which zsh 2>/dev/null)" ]]; then
     log "${INFO} Changing default shell to Zsh..."
-    chsh -s "$(which zsh)" 2>/dev/null || true
+    sudo chsh -s "$(which zsh)" "$USER" 2>/dev/null || true
     log "${OK} Default shell changed to Zsh"
 fi
 
